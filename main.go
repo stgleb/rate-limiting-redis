502c7526d42e1570d795ba8cb54b46990aceac83
Gleb <glebstepanov1992@gmail.com>
2017-02-09 14:35:01 +0200

    Add multi limits for go

--- main.go
+++ main.go
@@ -5,20 +5,35 @@ import (
 	//"os"
 	//"io/ioutil"
 	//"log"
-	"time"
 	"fmt"
+	"time"
 )
 
-func overLimit(client *redis.Client, resourceName string, duration int, limit int64) bool {
-	key := fmt.Sprintf("%s:%d:%d", resourceName, duration, time.Now().Unix())
+type Limit struct {
+	duration int64
+	count    int64
+}
+
+func overLimit(client *redis.Client, resourceName string, duration int64, limit int64) bool {
+	key := fmt.Sprintf("%s:%d:%d", resourceName, duration, time.Now().Unix()/duration)
 	count := client.Incr(key)
-	client.Expire(key, time.Duration(duration) * time.Second)
+	client.Expire(key, time.Duration(duration)*time.Second)
 
 	if count.Val() > limit {
-		return false
+		return true
 	}
 
-	return true
+	return false
+}
+
+func overLimitMulti(client *redis.Client, resourceName string, limits []Limit) bool {
+	for _, limit := range limits {
+		if overLimit(client, resourceName, limit.duration, limit.count) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func main() {
@@ -49,9 +64,16 @@ func main() {
 	//result, err := cmd.Result()
 	//log.Print(cmd.Val())
 	//log.Print(result, err)
+	limits := []Limit{{duration: 1, count: 1}, {duration: 60, count: 5}}
 
-	for i := 0;i < 10; i++ {
-		fmt.Println(overLimit(client, "resource", 1, 1))
+	counter := 0
+
+	for counter < 10 {
+		if overLimitMulti(client, "resource", limits) == false {
+			fmt.Println("Ok")
+			counter++
+		}
+
 		time.Sleep(time.Duration(1) * time.Second)
 	}
 }
